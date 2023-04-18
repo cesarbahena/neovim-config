fc1d8be275da4d2a1c6d1212fd49d4011b30762e
Cesar Bahena <cabahenac@gmail.com>
2023-04-18 00:16:04 -0600

    Almost all the plugins needed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,26 +5,26 @@ lsp.preset("recommended")
 lsp.ensure_installed({
   'tsserver',
   'rust_analyzer',
+  'lua_ls',
 })
-
 -- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+lsp.configure('lua_ls', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 })
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-e>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-i>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-e>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  -- ["<C-i>"] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
@@ -36,30 +36,30 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  lsp.default_keymaps({buffer = bufnr})
+  local opts = { buffer = bufnr, remap = false }
+  lsp.default_keymaps({ buffer = bufnr })
   lsp.buffer_autoformat()
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "k", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>d", "<cmd>Telescope diagnostics<cr>", opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>&", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
 end)
 
 lsp.format_on_save({
@@ -67,14 +67,14 @@ lsp.format_on_save({
     timeout_ms = 10000,
   },
   servers = {
-    ['null-ls'] = {'javascript', 'typescript'},
+    ['null-ls'] = { 'javascript', 'typescript' },
   }
 })
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
 
 local null_ls = require('null-ls')
--- lua/cesar/remap.lua
+++ lua/cesar/remap.lua
@@ -1,47 +1,154 @@
-
+local map = vim.keymap.set
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
-vim.keymap.set("n", "<leader>q", vim.cmd.wq)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Arrow motions
+map("", "m", "b")
+map("", "n", "j")
+map("", "e", "k")
+map("", "i", "w")
+map("", "l", "e")
+map("", "u", "h")
+map("", "y", "l")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Quick movement
+map("", "M", "B")
+map("", "N", "<C-d>zz")
+map("", "E", "<C-u>zz")
+map("", "I", "W")
+map("", "L", "E")
+map("", "U", "^")
+map("", "Y", "$")
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Insert mode
+map("", "d", "i") -- "Dis" text object
+map("", "D", "I")
+map("", "h", "a") -- "wHole" text object
+map("", "H", "A")
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- Modes
+map("n", "a", "v")
+map("n", "aa", "V")
+map("x", "a", "V")
+map("", "A", "V")
+map("", "<C-a>", "<C-v>")
+map("", ";", ":")
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- Cut, copy, paste, undo
+map("", "X", "dd")
+map("", "x", "d")
+map("", "xx", "dd")
+map("", "<Del>", [["_dl]])
+map("", "<S-Del>", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+map("", "c", "y")
+map("", "cc", "Y")
+map("", "C", "Y")
+map("n", "cv", "yyp")
 
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+map("n", "v", "p")
+map("n", "V", "P")
+map("x", "v", [["_dP]])
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+map("", "z", "u")
+map("", "Z", "<C-r>")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- Replace and search
+map("", "r", "c")
+map("", "R", "C")
+map("", "<BS>", "r")
+map("n", "?", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "b", "nzzzv")
+map("n", "B", "Nzzzv")
+map("", "p", ";")
+map("", "P", ",")
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+-- Buffer management
+map("n", "<leader>pe", vim.cmd.Ex)
+map("n", "<leader>w", vim.cmd.wa)
+map("n", "<leader>qq", vim.cmd.wqa)
+map("n", "q", vim.cmd.close)
+map("n", "<leader>f", vim.lsp.buf.format)
+map("n", "<leader><leader>", function()
+  vim.cmd("so")
 end)
 
+-- Window navigation
+map("", "<C-u>", "<C-o>")
+map("", "<C-y>", "<C-i>")
+map("", "w", "<C-w>")
 
+-- Line management
+map("v", "<C-n>", ":m '>+1<CR>gv=gv")
+map("v", "<C-e>", ":m '<-2<CR>gv=gv")
+map("n", "J", "mzJ`z")
+map("n", "gJ", "mzgJ`z")
+map("", ",", "z")
+map("", "<leader>o", "o<Esc>")
+map("", "<leader>O", "O<Esc>")
+
+-- Word text object
+map("", "adi", "viw")
+map("", "adI", "viW")
+map("", "ahi", "vaw")
+map("", "ahI", "vaW")
+map("", "rdi", "ciw")
+map("", "rdI", "ciW")
+map("", "rhi", "caw")
+map("", "rhI", "caW")
+map("", "xdi", "diw")
+map("", "xdI", "diW")
+map("", "xhi", "daw")
+map("", "xhI", "daW")
+map("", "cdi", "yiw")
+map("", "cdI", "yiW")
+map("", "chi", "yaw")
+map("", "chI", "yaW")
+
+-- Parentheses text object
+map("", "ado", "vib")
+map("", "adO", "viB")
+map("", "aho", "vab")
+map("", "ahO", "vaB")
+map("", "rdo", "cib")
+map("", "rdO", "ciB")
+map("", "rho", "cab")
+map("", "rhO", "caB")
+map("", "xdo", "dib")
+map("", "xdO", "diB")
+map("", "xho", "dab")
+map("", "xhO", "daB")
+map("", "cdo", "yib")
+map("", "cdO", "yiB")
+map("", "cho", "yab")
+map("", "chO", "yaB")
+
+-- Quotation marks and paragraph text object
+map("", "adm", [[vi"]])
+map("", "adM", [[vip]])
+map("", "ahm", [[va"]])
+map("", "ahM", [[vap]])
+map("", "rdm", [[ci"]])
+map("", "rdM", [[cip]])
+map("", "rhm", [[ca"]])
+map("", "rhM", [[cap]])
+map("", "xdm", [[di"]])
+map("", "xdM", [[dip]])
+map("", "xhm", [[da"]])
+map("", "xhM", [[dap]])
+map("", "cdm", [[yi"]])
+map("", "cdM", [[yip]])
+map("", "chm", [[ya"]])
+map("", "chM", [[yap]])
+
+-- Miscelaneous
+map("n", "k", "nop")
+map("", "Q", "q")
+map("n", "<leader>qn", "<cmd>cnext<CR>zz")
+map("n", "<leader>qe", "<cd>cprev<CR>zz")
+map("n", "<leader>n", "<cmd>lnext<CR>zz")
+map("n", "<leader>e", "<cmd>lprev<CR>zz")
+map("n", "<C-d>", "<C-a>")
+map("n", "<C-s>", "<C-x>")
+map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+map("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+map("n", "<leader>vp", "<cmd>e ~/.config/nvim/lua/cesar/packer.lua<CR>");
