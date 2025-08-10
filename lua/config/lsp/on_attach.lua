88d9efb798073544110873a6e3bcfc77f661a06e
cesarpromedic <cesar.bahena@gmail.com>
2025-08-09 22:00:15 -0600

    refactor(lsp): on_attach
    - Used the helper functions for keybindings for buffer with an
      attached language server.

--- lua/config/lsp/on_attach.lua
+++ lua/config/lsp/on_attach.lua
@@ -5,24 +5,20 @@ return function(_, bufnr)
   end
 
   -- Navigation (using existing keys)
-  map({ "Yes", vim.lsp.buf.definition, details = ', go to definition' })
-  map({ "Go to Declaration", vim.lsp.buf.declaration })
-  map({ "Go to Type", vim.lsp.buf.type_definition })
-  map({ "Go to References", vim.lsp.buf.references })
-  map({ "Go to Implementation", vim.lsp.buf.implementation })
+  map { 'Yes', vim.lsp.buf.definition, details = ', go to definition' }
+  map { 'Go to Declaration', vim.lsp.buf.declaration }
+  map { 'Go to Type', vim.lsp.buf.type_definition }
+  map { 'Go to References', vim.lsp.buf.references }
+  map { 'Go to Implementation', vim.lsp.buf.implementation }
 
   -- Actions (using existing keys)
-  map({ "Code Rename", vim.lsp.buf.rename })
-  map({ "Code Suggestions", vim.lsp.buf.code_action })
-  map({ "Code Format", vim.lsp.buf.format })
-
-  -- Diagnostics (using existing keys)
-  map({ "Next dx", vim.diagnostic.goto_next })
-  map({ "Prev dx", vim.diagnostic.goto_prev })
+  map { 'Code Rename', vim.lsp.buf.rename }
+  map { 'Code Suggestions', vim.lsp.buf.code_action }
+  map { 'Code Format', vim.lsp.buf.format }
 
   -- Hover and signature
-  map({ "Code Definition", vim.lsp.buf.hover })
-  map({ "Signature", vim.lsp.buf.signature_help })
+  map { 'Code Definition', vim.lsp.buf.hover }
+  map { 'Signature', vim.lsp.buf.signature_help }
 
   -- Enable completion triggered by <c-x><c-o>
   vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
