d35a79192f096f1515a757b55e9b1b1df61022a5
cesarbahena <cesarbahena@example.com>
2025-08-12 12:14:21 -0600

    refactor: Use modern Lua API for runtimepath manipulation
    - Replace vim.cmd with vim.opt.rtp:prepend for cleaner code

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Add backup path to runtimepath FIRST if BACKUP_PATH is set
 if vim.env.NVIM_BACKUP_PATH then
-  vim.cmd('set runtimepath^=' .. vim.env.NVIM_BACKUP_PATH)
+  vim.opt.rtp:prepend(vim.env.NVIM_BACKUP_PATH)
 end
 
 require 'core'
