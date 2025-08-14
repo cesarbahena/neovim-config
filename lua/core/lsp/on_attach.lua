5ff6235bf5784d4e87c7e8e2daddf6395261a1c2
cesarbahena <cesarbahena@example.com>
2025-08-14 08:51:00 -0600

    refactor: Update LSP on_attach to use new keymap system
    - Replace individual map() calls with unified keymap{} block
    - Add bufnr parameter for buffer-local mappings
    - Maintain existing LSP key bindings and functionality

--- lua/core/lsp/on_attach.lua
+++ lua/core/lsp/on_attach.lua
@@ -1,24 +1,23 @@
 return function(_, bufnr)
-  local function map(spec)
-    spec.buffer = bufnr
-    keymap(key(spec))
-  end
+  keymap {
+    bufnr = bufnr,
 
-  -- Navigation (using existing keys)
-  map { 'Yes', vim.lsp.buf.definition, details = ', go to definition' }
-  map { 'Go to Declaration', vim.lsp.buf.declaration }
-  map { 'Go to Type', vim.lsp.buf.type_definition }
-  map { 'Go to References', vim.lsp.buf.references }
-  map { 'Go to Implementation', vim.lsp.buf.implementation }
+    -- Navigation (using existing keys)
+    key { 'Yes', vim.lsp.buf.definition, details = ', go to definition' },
+    key { 'Go to Declaration', vim.lsp.buf.declaration },
+    key { 'Go to Type', vim.lsp.buf.type_definition },
+    key { 'Go to References', vim.lsp.buf.references },
+    key { 'Go to Implementation', vim.lsp.buf.implementation },
 
-  -- Actions (using existing keys)
-  map { 'Code Rename', vim.lsp.buf.rename }
-  map { 'Code Suggestions', vim.lsp.buf.code_action }
-  map { 'Code Format', vim.lsp.buf.format }
+    -- Actions (using existing keys)
+    key { 'Code Rename', vim.lsp.buf.rename },
+    key { 'Code Suggestions', vim.lsp.buf.code_action },
+    key { 'Code Format', vim.lsp.buf.format },
 
-  -- Hover and signature
-  map { 'Code Definition', vim.lsp.buf.hover }
-  map { 'Signature', vim.lsp.buf.signature_help }
+    -- Hover and signature
+    key { 'Code Definition', vim.lsp.buf.hover },
+    key { 'Signature', vim.lsp.buf.signature_help },
+  }
 
   -- Enable completion triggered by <c-x><c-o>
   vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
