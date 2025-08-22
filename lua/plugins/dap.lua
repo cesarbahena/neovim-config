090827a58a1bb05bf4a79c6c3271ec4a68e3bba2
cesarbahena <cesarbahena@example.com>
2025-08-22 10:00:44 -0600

    feat(dap): Reorganize debug UI layout
    - Move scopes and watches to left panel (70%/30% split)
    - Move stacks and breakpoints to right panel (70%/30% split)
    - Keep repl and console in bottom panel (50%/50% split)
    - Provides better visual organization with related panels grouped together

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -161,26 +161,26 @@ return {
       layouts = {
         {
           elements = {
-            { id = 'scopes', size = 0.6 },
-            { id = 'watches', size = 0.4 },
-          },
-          size = 50,
-          position = 'right',
-        },
-        {
-          elements = {
-            { id = 'stacks', size = 0.4 },
-            { id = 'breakpoints', size = 0.6 },
+            { id = 'scopes', size = 0.7 },
+            { id = 'watches', size = 0.3 },
           },
           size = 40,
           position = 'left',
         },
         {
           elements = {
-            { id = 'repl', size = 0.6 },
-            { id = 'console', size = 0.4 },
+            { id = 'stacks', size = 0.7 },
+            { id = 'breakpoints', size = 0.3 },
           },
-          size = 0.3,
+          size = 40,
+          position = 'right',
+        },
+        {
+          elements = {
+            { id = 'repl', size = 0.5 },
+            { id = 'console', size = 0.5 },
+          },
+          size = 10,
           position = 'bottom',
         },
       },
