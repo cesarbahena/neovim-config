cba04d0c1cba46a9503132b503ff9ab504fa0740
cesarbahena <cesarbahena@example.com>
2025-09-05 23:10:09 -0600

    feat(nvim): change oil to minifiles

---
+++ nvim/.config/nvim/lua/plugins/file_manager.lua
@@ -0,0 +1,29 @@
+return {
+  'nvim-mini/mini.files',
+  -- No need to copy this inside `setup()`. Will be used automatically.
+  opts = {
+    mappings = {
+      close = 'q',
+      go_in = 'o',
+      go_in_plus = '<cr>',
+      go_out = 'k',
+      go_out_plus = '.',
+      mark_goto = "'",
+      mark_set = 'm',
+      reset = '<BS>',
+      reveal_cwd = '@',
+      show_help = 'g?',
+      synchronize = 'w',
+      trim_left = '<',
+      trim_right = '>',
+    },
+    -- Customization of explorer windows
+    windows = {
+      -- Whether to show preview of file/directory under cursor
+      preview = true,
+    },
+  },
+  keys = {
+    key { 'file explorer', '<cmd>lua MiniFiles.open(vim.fn.getcwd())<cr>' },
+  },
+}
